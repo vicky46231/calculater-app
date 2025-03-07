da9546bebd4cb540f3274bf3fac9d71ee577064a
vicky46231 <amitdandina@gmail.com>
2025-03-07 15:47:08 +0530

    msg

--- git.sh
+++ git.sh
@@ -1,5 +1,8 @@
 echo "this is the example for git"
 echo "this is the second line in git"
 echo " this line is for deleting the file"
+<<<<<<< HEAD
 echo "this is the fourth line in git"
 echo "this is the example for git reset"
+=======
+>>>>>>> parent of 0f3637d (msg)
